8971098ba6d40d7d660eff4da28b8ef210262be0
Anamawl <adekm1999@gmail.com>
2025-05-08 12:36:18 +0200

    new version main.cpp

--- cpp_modules/main_cpp.cpp
+++ cpp_modules/main_cpp.cpp
@@ -1,120 +1,16 @@
 #include <iostream>
 #include <iomanip>
-//#include <pybind11/pybind11.h> //Działa
-
 #include <windows.h>
 #include <tlhelp32.h>
-#include <tchar.h>
-#include <stdio.h>
+#include <psapi.h>
 
-BOOL GetProcessList();
-BOOL ListProcessModules(DWORD dwPID);
-// BOOL ListProcessThreads(Dword dwOwnerPID);
-void printError(TCHAR const* msg);
-//namespace py = pybind11;
+// #include <pybind11/pybind11.h>
+// namespace py = pybind11;
 
-int main(void)
-{
-    GetProcessList();
-    return 0;
+void printError(const wchar_t* msg) {
+    std::wcerr << L"Błąd: " << msg << L" (kod " << GetLastError() << L")" << std::endl;
 }
 
-BOOL GetProcessList()
-{
-    HANDLE hProcessSnap;
-    HANDLE hProcess;
-    PROCESSENTRY32 pe32;
-    DWORD dwPriorityClass;
-
-    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if(hProcessSnap == INVALID_HANDLE_VALUE)
-    {
-        printError(L"CreateToolhelp32Snapshot (of processes)");
-        return(FALSE);
-    }
-
-    pe32.dwSize = sizeof(PROCESSENTRY32);
-
-    if(!Process32First(hProcessSnap, &pe32))
-    {
-        printError(L"Process32First");
-        CloseHandle(hProcessSnap);
-        return(FALSE);
-    }
-
-    do
-    {
-        std::wcout << L">PROCESS NAME: " << pe32.szExeFile << std::endl;
-
-        dwPriorityClass = 0;
-        hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
-        if(hProcess == NULL)
-            printError(L"OpenProcess");
-        else
-        {
-            dwPriorityClass = GetPriorityClass(hProcess);
-            if(!dwPriorityClass)
-                printError(L"GetPriorityClass");
-            CloseHandle(hProcess);
-        }
-
-        std::wcout << L" Process ID        = "<< std::setw(8) << std::setfill(L'0') << pe32.th32ProcessID << std::endl;
-        std::wcout << L" Thread count      = "<< std::dec << pe32.cntThreads << std::endl;
-        std::wcout << L" Parent process ID = "<< std::setw(8) << std::setfill(L'0') << pe32.th32ParentProcessID << std::endl;
-        std::wcout << L" Priority base     = "<< std::dec << pe32.pcPriClassBase << std::endl;
-        if(dwPriorityClass)
-        std::wcout << L" Priority class    = "<< std::dec << dwPriorityClass << std::endl;
-        
-        //ListProcessModules( pe32.th32ProcessID );
-        //ListProcessThreads( pe32.th32ProcessID );
-        
-    } while(Process32Next(hProcessSnap, &pe32));
-    
-    CloseHandle(hProcessSnap);
-    
-    
-    return(TRUE);
-}
-
-BOOL ListProcessModules(DWORD dwPID)
-{
-    HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
-    MODULEENTRY32 me32;
-    
-    hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
-    if(hModuleSnap == INVALID_HANDLE_VALUE)
-    {
-        printError(L"CreateToolhelp32Snapshot (of modules)");
-        return(FALSE);
-    }
-    
-    me32.dwSize = sizeof(MODULEENTRY32);
-    
-    if(!Module32First(hModuleSnap, &me32))
-    {
-        printError(L"Module32First");
-        CloseHandle(hModuleSnap);
-        return(FALSE);
-    }
-    do
-    {
-        std::wcout << L"\n   Module Name:    " << me32.szModule << std::endl; //%s
-        std::wcout << L"   Executable      =" << me32.szExePath << std::endl; //%s
-        std::wcout << L"   Process ID      =" << std::setw(8) << std::setfill(L'0') << me32.th32ProcessID << std::endl;
-        std::wcout << L"   Ref count (g)   =" << std::setw(4) << std::setfill(L'0') << me32.GlblcntUsage << std::endl;
-        std::wcout << L"   Ref count (p)   =" << std::setw(4) << std::setfill(L'0') << me32.ProccntUsage << std::endl;
-        std::wcout << L"   Base address    =" << std::setw(8) << std::setfill(L'0') << me32.th32ProcessID << std::endl;
-
-
-    }while(Module32Next(hModuleSnap, &me32));
-
-
-    CloseHandle(hModuleSnap);
-    return(TRUE);
-}
-
-
-//----------------------------------------
 int dodaj(int a, int b) {
     return a + b;
 }
@@ -123,6 +19,62 @@ int dodaj(int a, int b) {
 //     m.def("dodaj", &dodaj, "Dodaje dwie liczby");
 // }
 
-void printError(const wchar_t* msg) {
-    std::wcerr << L"Błąd: " << msg << L" (kod " << GetLastError() << L")" << std::endl;
+int main() {
+    HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE) {
+        printError(L"CreateToolhelp32Snapshot (of processes)");
+        return 1;
+    }
+
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    if (!Process32First(hProcessSnap, &pe32)) {
+        printError(L"Process32First");
+        CloseHandle(hProcessSnap);
+        return 1;
+    }
+
+    do {
+        std::wcout << L">PROCESS NAME: " << pe32.szExeFile << std::endl;
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe32.th32ProcessID);
+
+        if (hProcess != NULL) {
+            // RAM usage
+            PROCESS_MEMORY_COUNTERS pmc;
+            if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
+                std::wcout << L" RAM (WorkingSet)   = " << pmc.WorkingSetSize / 1024 << L" KB" << std::endl;
+            }
+
+            // CPU time
+            FILETIME creationTime, exitTime, kernelTime, userTime;
+            if (GetProcessTimes(hProcess, &creationTime, &exitTime, &kernelTime, &userTime)) {
+                ULARGE_INTEGER uKernelTime, uUserTime;
+                uKernelTime.LowPart = kernelTime.dwLowDateTime;
+                uKernelTime.HighPart = kernelTime.dwHighDateTime;
+                uUserTime.LowPart = userTime.dwLowDateTime;
+                uUserTime.HighPart = userTime.dwHighDateTime;
+
+                auto totalCpuTimeMs = (uKernelTime.QuadPart + uUserTime.QuadPart) / 10000;
+                std::wcout << L" CPU time           = " << totalCpuTimeMs << L" ms" << std::endl;
+            }
+
+            // Disk I/O
+            IO_COUNTERS ioCounters;
+            if (GetProcessIoCounters(hProcess, &ioCounters)) {
+                std::wcout << L" IO Read            = " << ioCounters.ReadTransferCount / 1024 << L" KB" << std::endl;
+                std::wcout << L" IO Write           = " << ioCounters.WriteTransferCount / 1024 << L" KB" << std::endl;
+            }
+
+            CloseHandle(hProcess);
+        } else {
+            printError(L"OpenProcess");
+        }
+
+        std::wcout << std::endl;
+
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    CloseHandle(hProcessSnap);
+    return 0;
 }
