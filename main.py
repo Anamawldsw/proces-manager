4c9dbb90d5972248ae4aba2383ac71a141ecefb6
Anamawl <adekm1999@gmail.com>
2025-05-29 09:21:44 +0200

    update CPU data add monitor launch for opening in new window

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'cpp_modules'))
 import main_cpp
 
 
-
 try:
     while True:
+        prev_cpu = main_cpp.get_total_cpu_time()
+        prev_uptime = main_cpp.get_uptime_ms()
+
+        time.sleep(1)
+
         main_cpp.init_all_metrics()
 
         ram_used = main_cpp.get_total_ram_used_mb()
-        cpu_time = main_cpp.get_total_cpu_time()
+        curr_cpu = main_cpp.get_total_cpu_time()
+        curr_uptime = main_cpp.get_uptime_ms()
         io_read = main_cpp.get_total_io_read_mb()
         io_write = main_cpp.get_total_io_write_mb()
-        uptime_ms = main_cpp.get_uptime_ms()
         num_cores = main_cpp.get_cpu_count()
 
-        cpu_percent = (cpu_time / (uptime_ms * num_cores)) * 100 if uptime_ms > 0 else 0
+        cpu_delta = curr_cpu - prev_cpu
+        time_delta = (curr_uptime - prev_uptime) * num_cores
+        cpu_percent = (cpu_delta / time_delta) * 100 if time_delta > 0 else 0
+
 
         
         print("\033[2J", end="")
@@ -28,19 +35,19 @@ try:
         
         print("=" * 25, "SYSTEM TOTAL", "=" * 25, flush=True)
         print(f" Total RAM used      = {ram_used} MB", flush=True)
-        print(f" Total CPU time      = {cpu_time} ms ({cpu_percent:.2f}%)", flush=True)
+        print(f" Total CPU time      = {curr_cpu} ms ({cpu_percent:.2f}%)", flush=True)
         print(f" Total IO Read       = {io_read} MB", flush=True)
         print(f" Total IO Write      = {io_write} MB", flush=True)
 
 
-        processes = main_cpp.get_process_list()
-        print("=" * 25, "PROCESS LIST", "=" * 25)
-        for proc in processes:
-            print(f"{proc['pid']:>5} | {proc['name']:<40} | "
-                f"RAM: {proc.get('ram_kb', 0):<10} KB | "
-                f"CPU: {proc.get('cpu_time_ms', 0):<10} ms | "
-                f"IO Read: {proc.get('io_read_kb', 0):<10} KB | "
-                f"IO Write: {proc.get('io_write_kb', 0):<10} KB")
+        # processes = main_cpp.get_process_list()
+        # print("=" * 25, "PROCESS LIST", "=" * 25)
+        # for proc in processes:
+        #     print(f"{proc['pid']:>5} | {proc['name']:<40} | "
+        #         f"RAM: {proc.get('ram_kb', 0):<10} KB | "
+        #         f"CPU: {proc.get('cpu_time_ms', 0):<10} ms | "
+        #         f"IO Read: {proc.get('io_read_kb', 0):<10} KB | "
+        #         f"IO Write: {proc.get('io_write_kb', 0):<10} KB")
         time.sleep(0.5)
 
 
---
+++ monitor_launch.py
@@ -0,0 +1,4 @@
+import os
+
+script_path = os.path.abspath("main.py")
+os.system(f'start cmd /k python "{script_path}"')
