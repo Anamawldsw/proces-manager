5900e8ae300dfd39269b9e649c32bf43ba153b42
Anamawl <adekm1999@gmail.com>
2025-06-03 12:46:06 +0200

    process list print

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'cpp_modules'))
 
 import main_cpp
 
-
+temp = True
 try:
-    while True:
+    while temp:
         prev_cpu = main_cpp.get_total_cpu_time()
         prev_uptime = main_cpp.get_uptime_ms()
 
@@ -33,23 +33,23 @@ try:
         print("\033[2J", end="")
         print("\033[H", end="")
         
-        print("=" * 25, "SYSTEM TOTAL", "=" * 25, flush=True)
+        print("=" * 25, "SYSTEM TOTAL", "=" * 102, flush=True)
         print(f" Total RAM used      = {ram_used} MB", flush=True)
         print(f" Total CPU time      = {curr_cpu} ms ({cpu_percent:.2f}%)", flush=True)
         print(f" Total IO Read       = {io_read} MB", flush=True)
         print(f" Total IO Write      = {io_write} MB", flush=True)
 
 
-        # processes = main_cpp.get_process_list()
-        # print("=" * 25, "PROCESS LIST", "=" * 25)
-        # for proc in processes:
-        #     print(f"{proc['pid']:>5} | {proc['name']:<40} | "
-        #         f"RAM: {proc.get('ram_kb', 0):<10} KB | "
-        #         f"CPU: {proc.get('cpu_time_ms', 0):<10} ms | "
-        #         f"IO Read: {proc.get('io_read_kb', 0):<10} KB | "
-        #         f"IO Write: {proc.get('io_write_kb', 0):<10} KB")
+        processes = main_cpp.get_process_list()
+        print("=" * 25, "PROCESS LIST", "=" * 102)
+        for proc in processes:
+            print(f"{proc['pid']:>5} | {proc['name']:<40} | "
+                f"RAM: {proc.get('ram_kb', 0):<10} KB | "
+                f"CPU: {proc.get('cpu_time_ms', 0):<10} ms | "
+                f"IO Read: {proc.get('io_read_kb', 0):<10} KB | "
+                f"IO Write: {proc.get('io_write_kb', 0):<10} KB")
         time.sleep(0.5)
 
-
+        temp = False
 except KeyboardInterrupt:
     print("\nMonitoring zakończony.")
